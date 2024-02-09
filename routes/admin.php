658e45bd1de14c1e2575ad57fa60f35b095711b6
maytech76 <yanezmarco0576@gmail.com>
2024-02-09 11:24:03 -0400

    09-02-2024 Add Components, Views, layout, Provider -> Admin

--- routes/admin.php
+++ routes/admin.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/' , function(){
-   return "dashdboard Admin";
-})->name('Dashboard');
+
+   return view('admin.dashboard');
+   
+})->name('dashboard');
