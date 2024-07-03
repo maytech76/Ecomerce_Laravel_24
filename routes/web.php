f260d73de58aa89212eb9d83274ff6c62b3a096c
maytech76 <yanezmarco0576@gmail.com>
2024-07-02 21:55:54 -0500

    02-06-24 config login and logout is route '/welcome'

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/welcome', function () {
+        return view('welcome');
+    })->name('welcome');
 });
 
 
