5b34f220d588d9cd9ba9c30dc56e62b55d93fc66
maytech76 <yanezmarco0576@gmail.com>
2024-07-07 18:33:54 -0500

    07-07-24 add efect dropdrown in menu options

--- resources/views/livewire/filter.blade.php
+++ resources/views/livewire/filter.blade.php
@@ -1,18 +1,26 @@
 <div class="bg-white py-12">
 
     <x-container class="px-4 flex">
+
         <aside class="w-52 flex-shrink-0 mr-8">
              <ul class="space-y-6">
                  @foreach ( $options as $option )
 
-                     <li>
-                       <button class="px-4 py-2 bg-slate-300 w-full text-left text-gray-800 flex justify-between items-center">
+                     <li x-data="{open: true}">
+                       <button class="rounded-sm px-4 py-2 bg-slate-300 w-full text-left text-gray-800 flex justify-between items-center" x-on:click="open = !open">
+
                           {{ $option->name }}
 
-                           <i class="fa-solid fa-angle-down"></i>
+                           <i class="fa-solid fa-angle-down"
+                             x-bind:class="{
+                                'fa-solid fa-angle-down': open,
+                                'fa-solid fa-angle-right': !open
+                             }"
+                             ></i>
+
                        </button>
 
-                        <ul class="mt-2 space-y-2">
+                        <ul class="mt-2 space-y-2" x-show="open">
                             @foreach ( $option->features as $feature )
                                 <li>
                                     <label class="inline-flex items-center">
