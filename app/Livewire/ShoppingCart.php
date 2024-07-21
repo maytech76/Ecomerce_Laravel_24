c6d6e7eaeeb246c4058c314df8070900e3d055da
maytech76 <yanezmarco0576@gmail.com>
2024-07-20 23:29:16 -0500

    20-07-24 Add Reactive Buttons Eliminar, Restar, sumar, Vaciar Carrito

--- app/Livewire/ShoppingCart.php
+++ app/Livewire/ShoppingCart.php
@@ -14,6 +14,93 @@ class ShoppingCart extends Component
    }
 
 
+   /* METODO PARA INCREMENTAR LA CANTIDAD POR ITEMS */
+   public function increase($rowId)
+   {
+     /* Definimos la instancia con la que trabajamos */
+     Cart::instance('shopping');
+
+     /* Almacenamos el identificador rowId que viene del blade en item */
+     $item = Cart::get($rowId);
+
+    /*  Aplicaos el metodo update, al regitro selecionado y sumamo 1 */
+     Cart::update($rowId, $item->qty +1);
+
+     /* Aplicamos un chequeo, si el usuario se encuentra autentificado permite add to cart */
+     if (auth()->check()){
+      Cart::store(auth()->id());
+     }
+
+      /* Declaramos un evento que emitira el conteo de los items del carrito */
+     $this->dispatch('cartUpdated', Cart::count());
+      /* Este Evento sera escuchado por navigation.blade, donde se encuentra el icono cart */
+   }
+
+   /* METODO PARA DESCONTAR LA CANTIDAD POR ITEMS */
+   public function decrease($rowId)
+   {
+      /* Definimos la instancia con la que trabajamos */
+     Cart::instance('shopping');
+
+     /* Almacenamos el identificador rowId que viene del blade en item */
+     $item = Cart::get($rowId);
+
+    if ($item->qty > 1 ) { /* Si el valor de qty es mayor a 1 restamos 1 */
+      /*  Aplicaos el metodo update, al regitro selecionado y sumamo 1 */
+       Cart::update($rowId, $item->qty - 1);
+    }else{
+       
+       /* De lo contrario removemos del carrito el item selecionado */
+       Cart::remove($rowId);
+    }
+
+     /* Aplicamos un chequeo, si el usuario se encuentra autentificado permite add to cart */
+     if (auth()->check()){
+      Cart::store(auth()->id());
+     }
+
+      /* Declaramos un evento que emitira el conteo de los items del carrito */
+     $this->dispatch('cartUpdated', Cart::count());
+      /* Este Evento sera escuchado por navigation.blade, donde se encuentra el icono cart */
+   }
+
+   /* METODO PARA REMOVER ITEM SELECIONADO DEL CARRITO */
+   public function remove($rowId)
+   {
+      Cart::instance('shopping');
+      Cart::remove($rowId);
+
+      /* Aplicamos un chequeo, si el usuario se encuentra autentificado permite add to cart */
+     if (auth()->check()){
+      Cart::store(auth()->id());
+     }
+
+      /* Declaramos un evento que emitira el conteo de los items del carrito */
+     $this->dispatch('cartUpdated', Cart::count());
+      /* Este Evento sera escuchado por navigation.blade, donde se encuentra el icono cart */
+
+      
+   }
+
+
+   /* METODO PARA VACIAR POR COMPLETO EL CARRITO */
+   public function destroy()
+   {
+     Cart::instance('shopping');
+     Cart::destroy();
+
+     /* Aplicamos un chequeo, si el usuario se encuentra autentificado permite add to cart */
+     if (auth()->check()){
+      Cart::store(auth()->id());
+     }
+
+      /* Declaramos un evento que emitira el conteo de los items del carrito */
+     $this->dispatch('cartUpdated', Cart::count());
+      /* Este Evento sera escuchado por navigation.blade, donde se encuentra el icono cart */
+
+   }
+
+
     public function render()
     {
         return view('livewire.shopping-cart');
--- resources/views/livewire/shopping-cart.blade.php
+++ resources/views/livewire/shopping-cart.blade.php
@@ -9,7 +9,8 @@
                     Carrito de compras (<p class="font-bold text-red-600 mx-1">{{Cart::count()}}</p> Productos)
                 </h1>
 
-                <button class="font-semibold text-gray-500 hover:text-red-600 hover:underline">
+                <button class="font-semibold text-gray-500 hover:text-red-600 hover:underline"
+                    wire:click="destroy()">
                     Vaciar Carrito
                 </button>
            </div>
@@ -30,26 +31,30 @@
                                     {{$item->name}}
                                 </a> 
                             </p>
-                            <button class="bg-red-100 hover:bg-red-300 text-red-800 text-sm font-semibold rounded px-2">
+
+                            <button class="bg-red-100 hover:bg-red-300 text-red-800 text-sm font-semibold rounded px-2"
+                                wire:click="remove('{{$item->rowId}}')">
                                 <i class="fa-solid fa-xmark"></i>
                                 Quitar
                             </button>
                          </div>
 
+                        {{--  Precio x item --}}
                          <p class="flex font-semibold mr-2">
                            {{$item->price}} 
                          </p>
                          <span>CLP</span>
 
+                          {{-- Cantidades --}}
                          <div class="ml-auto space-x-2">
 
-                              <button class="btn btn-gris" x-on:click="qty = qty - 1">
+                              <button class="btn btn-gris" wire:click="decrease('{{$item->rowId}}')">
                                  -
                               </button>
             
                               <span class="inline-block w-2 text-center">{{$item->qty}} </span>
             
-                              <button class="btn btn-gris">
+                              <button class="btn btn-gris" wire:click="increase('{{$item->rowId}}')">
                                  +
                               </button>
 
@@ -58,7 +63,16 @@
 
                       <hr>
                     @empty
-                      <p class="text-center">No hay Productos en el Carrito</p>            
+                      <p class="text-center">No hay Productos en el Carrito</p>
+                      <div class="flex items-center p-4 mb-4 text-sm text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 dark:border-yellow-800" role="alert">
+                        <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                        </svg>
+                        <span class="sr-only">Info</span>
+                        <div>
+                          <span class="font-medium">No se puede Proceder, </span> Debes Agregar Productos al carrito para poder continuar..!
+                        </div>
+                      </div>            
                    @endforelse
               </ul>
            </div>
